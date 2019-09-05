16ca2b608ea90066dc31f2f07ef2ba67cad648bf
Ash Todd <ashztodd@gmail.com>
2019-09-05 10:51:43 -0700

    Finished Part 1

---
+++ benchBnB/frontend/bench_bnb.jsx
@@ -0,0 +1,32 @@
+// frontend/bench_bnb.jsx
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import configureStore from './store/store.js';
+
+import Root from "./components/root";
+
+// –––testing–––
+// import { signup, login, logout } from './util/session_api_util';
+import { signup, login, logout } from './actions/session_actions';
+
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store}/>, root);
+});
